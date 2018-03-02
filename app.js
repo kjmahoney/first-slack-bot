751f82f0b355f444c63178afcae711abfe0af7ed
Kevin Mahoney <kmahoney@theatlantic.com>
2018-03-02 16:39:37 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.status(200).send('hello world!');
+})
+
+app.listen(port, () => {
+  console.log('listening on port' + port);
+})
+
+app.post('/hello', (req,res, next) => {
+  let userName = req.body.user_name;
+  userName = userName.charAt(0).toUpperCase() + userName.slice(1);
+  let botPayload = {
+    text: 'Hello ' +userName
+  }
+  if (userName !== 'Slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+})
