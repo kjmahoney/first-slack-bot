291a132f70394083bd6bcfe4ab0bf6cb32f5d980
Kevin Mahoney <kmahoney@theatlantic.com>
2018-03-23 17:41:22 -0400

    edit return

--- app.js
+++ app.js
@@ -15,21 +15,6 @@ app.listen(port, () => {
   console.log('listening on port' + port);
 })
 
-app.post('/hello', (req,res, next) => {
-  let userName = req.body.user_name;
-  userName = userName.charAt(0).toUpperCase() + userName.slice(1);
-  let botPayload = {
-    text: 'Helloooooo ' +userName
-  }
-  if (userName !== 'Slackbot') {
-    return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
-  }
-})
-
-//first one says, hell no i know you aint talking behind my back"
-//
 const exclamArray = [
   "oh helllll no",
   "HELL no",
